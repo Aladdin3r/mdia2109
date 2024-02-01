6065d2669220fdd1d842fc87853670494a6e218d
Rawan Aladdin <raladdin@se14l0124-13.ad.bcit.ca>
2024-02-01 15:05:30 -0800

    week 5 class

--- data-example-3/pages/index.js
+++ data-example-3/pages/index.js
@@ -1,15 +1,24 @@
 import Head from "next/head";
 import styles from "@/styles/Home.module.css";
-
+import books from "@/data/inventory.json"
+import { useState } from "react";
 
 export default function Home() {
+
+  const [data, setData] = useState(books);
+  console.log(books);
   return (
     <>
-      <Head>
-        
-      </Head>
       <main className={`${styles.main}`}>
-        
+        {
+          data && data.map((i, index) => {
+            return(
+              <div key={index}>
+                {i.yearPublished}
+              </div>
+            )
+          })
+        }
       </main>
     </>
   );
