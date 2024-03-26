f1a29016575d15f7b5a3f21690e3bcc1de3df96b
Aladdin3r <raladdin@my.bcit.ca>
2024-03-26 16:53:26 -0700

    trying to get form tests to pass

--- a01386700_aladdin_assessment_4/components/Form/index.js
+++ a01386700_aladdin_assessment_4/components/Form/index.js
@@ -14,22 +14,30 @@ export default function Form() {
         event.preventDefault();
         setFormSubmitted(true);
     };
+
     const handleCheckboxChange = (event) => {
         setIsChecked(event.target.checked);
     };
+
     const handleFirstNameChange = (event) => {
         setFirstName(event.target.value);
     };
+
     const handleLastNameChange = (event) => {
         setLastName(event.target.value);
     };
+
     const handleEmailChange = (event) => {
         setEmail(event.target.value);
     };
+
     const shouldShowSubmitButton = () => {
-        return firstName.match(nameRegex) && firstName.length > 1 &&
-            lastName.match(nameRegex) && lastName.length > 1 &&
-            email.match(emailRegex) && isChecked;
+        return firstName.match(nameRegex) &&
+            firstName.length > 1 &&
+            lastName.match(nameRegex) &&
+            lastName.length > 1 &&
+            email.match(emailRegex) &&
+            isChecked;
     };
 
     return (
@@ -38,15 +46,15 @@ export default function Form() {
                 <>
                     <p className={styles.formlabel}>Have any questions?</p>
                     <form className={styles.form} onSubmit={handleSubmit}>
-                        <label>First Name:</label>
-                        <input type="text" name="fname" value={firstName} onChange={handleFirstNameChange} className={styles.input}/>
-                        <label>Last Name:</label>
-                        <input type="text" name="lname" value={lastName} onChange={handleLastNameChange} className={styles.input}/>
-                        <label>Email:</label>
-                        <input type="email" name="email" value={email} onChange={handleEmailChange} className={styles.input}/>
+                        <label htmlFor="fname">First Name:</label>
+                        <input type="text" id="fname" name="fname" value={firstName} onChange={handleFirstNameChange} className={styles.input}/>
+                        <label htmlFor="lname">Last Name:</label>
+                        <input type="text" id="lname" name="lname" value={lastName} onChange={handleLastNameChange} className={styles.input}/>
+                        <label htmlFor="email">Email:</label>
+                        <input type="email" id="email" name="email" value={email} onChange={handleEmailChange} className={styles.input}/>
                         <div className={styles.checkboxcontainer}>
-                            <label className={styles.checkboxlabel}>Do you agree to the terms?:</label>
-                            <input type="checkbox" name="check" checked={isChecked} onChange={handleCheckboxChange} className={styles.input}/>
+                            <label htmlFor="termsCheckbox" className={styles.checkboxlabel}>Do you agree to the terms?</label>
+                            <input type="checkbox" id="termsCheckbox" name="check" checked={isChecked} onChange={handleCheckboxChange} className={styles.input}/>
                         </div>
                         {shouldShowSubmitButton() && (
                             <input type="submit" value="Submit" className={styles.submit}/>
